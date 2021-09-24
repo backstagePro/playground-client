2a048cadf8f2a3a81fc4fd785515d9dadd92db34
nexy <niki.kushlev@gmail.com>
2021-09-24 12:23:10 +0300

    Adding projects link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,60 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'antd/dist/antd.css';
 import type { AppProps } from 'next/app'
+import Link from 'next/link';
+
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <Layout>
+      {/* <Header className="header">
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+          <Menu.Item key="1">nav 1</Menu.Item>
+        </Menu>
+      </Header> */}
+      <Layout>
+        <Sider width={130} className="site-layout-background">
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%', borderRight: 0 }}
+          >
+
+            
+            <Menu.Item key="1">
+              <Link href="/projects">Projects</Link>
+            </Menu.Item> 
+
+            {/* <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+              <Menu.Item key="5">option5</Menu.Item>
+              <Menu.Item key="6">option6</Menu.Item>
+              <Menu.Item key="7">option7</Menu.Item>
+              <Menu.Item key="8">option8</Menu.Item>
+            </SubMenu> */}
+          </Menu>
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Content
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+            }}
+          >
+            <Component {...pageProps} />
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
+  );
 }
 export default MyApp
---
+++ pages/projects/index.tsx
@@ -0,0 +1,12 @@
+
+interface IProps {
+
+}
+
+const Index: React.FC<IProps> = ({}) => {
+    return (
+      <div> Projects </div>
+    )
+}
+
+export default Index;
